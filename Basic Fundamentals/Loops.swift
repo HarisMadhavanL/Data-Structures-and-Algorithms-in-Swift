fd732e0e30d05ed051a8205ba93b3cda6c7d61b7
HarisMadhavanL <119914447+HarisMadhavanL@users.noreply.github.com>
2024-12-01 20:23:38 +0530

    Create Loops

---
+++ Basic Fundamentals/Loops.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+// Task 1:- Use a for-in loop to print the numbers from 1 to 10.
+for i in 1...10 {
+    print(i)
+}
+
+
+// Task 2:- Use a while loop to print the numbers from 10 down to 1.
+var count = 10
+
+while count > 0 {
+    print(count)
+    count -= 1
+}
