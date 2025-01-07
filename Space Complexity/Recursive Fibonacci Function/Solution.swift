c083aeddd7cc81878eb8c76d0834f69a61c1afe9
HarisMadhavanL <119914447+HarisMadhavanL@users.noreply.github.com>
2025-01-07 09:30:04 +0530

    Delete Space Complexity/Recursive Fibonacci Function directory

--- Space Complexity/Recursive Fibonacci Function/Solution.swift
+++
@@ -1,9 +0,0 @@
-func fibonacciRecursive(_ n: Int) -> Int {
-    if n <= 1 {
-        return n
-    }
-    return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2)
-}
-
-let result = fibonacciRecursive(5) 
-print("Recursive Fibonacci Result: \(result)")
